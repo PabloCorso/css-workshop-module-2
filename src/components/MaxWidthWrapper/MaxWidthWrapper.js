0dc9f692eb467c2a5bb98474e467aa48e16e3f71
Pablo Corso <pabcorso@gmail.com>
2022-05-20 18:53:30 +0200

    2 - layout

--- src/components/MaxWidthWrapper/MaxWidthWrapper.js
+++ src/components/MaxWidthWrapper/MaxWidthWrapper.js
@@ -1,17 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import styles from './MaxWidthWrapper.module.css';
+import styles from "./MaxWidthWrapper.module.css";
 
-const MaxWidthWrapper = ({
-  children,
-  className = '',
-  ...delegated
-}) => {
+const MaxWidthWrapper = ({ children, className = "", ...delegated }) => {
   return (
-    <div
-      className={`${styles.maxWidthWrapper} ${className}`}
-      {...delegated}
-    >
+    <div className={`${styles.maxWidthWrapper} ${className}`} {...delegated}>
       {children}
     </div>
   );
